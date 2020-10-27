2115256fdb851faf80557205ad7b6b2281bdf320
Raju Kumar <40966657+kraju2742@users.noreply.github.com>
2020-10-27 18:04:54 +0530

    Add files via upload

---
+++ evenr/script.js
@@ -0,0 +1,9 @@
+
+function fun(){
+           document.getElementById("main").style.display = "none";
+           document.getElementById("myModal").style.display = "block";
+       }
+
+function fun2(){
+    document.getElementById("main").style.display = "none";
+}
